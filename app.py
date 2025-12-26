ac99fd1e51513e14be7bfc802ba917ae392bdf1a
Andrew <arvizumunozandrew@gmail.com>
2025-12-25 18:33:35 -0800

    connection between 3 components set up still need file->apple

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ from flask_cors import CORS
 from PIL import Image
 import io
 import numpy as np
+import subprocess
+import csv
+import requests
+from dotenv import load_dotenv
+import os
+import config
+from io import BytesIO
 
 app = Flask(__name__)
 
@@ -11,10 +18,68 @@ CORS(app) #add origins When frontend is up
 BLACK = (0,0,0)
 GRAY = (51,51,51)
 
-
-def process_icon(image_file, target_size=1024):
+#ideviceisntaller
+def get_app_names():
     
-    img = Image.open(image_file).convert("RGBA")
+    load_dotenv()
+    IDEVICEINSTALLER_PATH = os.getenv('IDEVICEINSTALLER_PATH')
+    result = subprocess.run( [IDEVICEINSTALLER_PATH, "list"], capture_output=True, text=True, check=False )
+    
+    app_names = []
+    
+    with io.StringIO(result.stdout) as f:
+        reader = csv.reader(f)
+        
+        next(reader, None)
+        
+        for row in reader:
+            if len(row) == 3:
+                app_names.append( row[2].strip().strip('"'))
+            
+    return app_names
+
+#itunes
+def get_icons():
+    app_names = get_app_names()
+    url = config.BASE_URL
+
+    icon_urls = []
+
+    for name in app_names:
+        
+    
+    
+        params = {
+        "term" : name,
+        "entity" : config.ENTITY,
+        "media" : config.MEDIA,
+        "country" : config.COUNTRY,
+        "limit" : config.LIMIT
+        }
+        
+        try:
+            response = requests.get(url, params=params)
+            
+            if response.status_code == 200:
+                icon_url = response.json()
+                
+                icon_urls.append(icon_url["results"][0]["artworkUrl512"])
+            else:
+                print("Error: ", response.status_code)
+            
+        except requests.RequestException as e:
+            print("error", e)
+    
+    return icon_urls    
+    
+    
+    
+def process_icon(image, target_size=1024):
+    
+    #img = Image.open(image_file).convert("RGBA")
+    img = Image.open(BytesIO(image)).convert("RGBA")
+
+    
     img = img.resize((target_size, target_size), Image.Resampling.LANCZOS)
     
     img_np = np.array(img) #PIL to NP for speedup (hieght, width, 4)
@@ -32,34 +97,52 @@ def process_icon(image_file, target_size=1024):
     
     return Image.fromarray(final_img, mode="RGBA")
 
+
  
 @app.route("/process-icon", methods=["POST"])
 def process_icon_endpoint():
-    try:
-        if "icon" not in request.files:
-            print("STUCK")
-            return jsonify({"error":"Noicon file provided"}), 400
-
-        file = request.files["icon"]
-        
-        if file.filename == "":
-            return jsonify({"error":"Nofilename provided"}), 400
-        
-        processed_img = process_icon(file, target_size=1024)
-        
-        img_io = io.BytesIO()
-        processed_img.save(img_io, "PNG", optimize=True)
-        img_io.seek(0)
-        
-        return send_file(img_io, mimetype="image/png")        
-        
-        
-    except Exception as e:
-        print("Error: ", e)
-        return jsonify({"error": str(e)}), 500
     
+    icon_urls = get_icons()
+    icon_imgs = []
     
-    
+    for icon_url in icon_urls:
+        response = requests.get(icon_url)
+        response.raise_for_status()
+        
+        #img = Image.open(BytesIO(response.content))
+            
+            
+        try:
+            '''
+            if "icon" not in request.files:
+                print("STUCK")
+                return jsonify({"error":"Noicon file provided"}), 400
+            
+            file = request.files["icon"]
+            
+            if file.filename == "":
+                return jsonify({"error":"Nofilename provided"}), 400
+            '''
+            
+            processed_img = process_icon(response.content, target_size=1024)
+            
+            img_io = io.BytesIO()
+            processed_img.save(img_io, "PNG", optimize=True)
+            img_io.seek(0)
+            
+            #returns a single image currently 
+            
+            return send_file(img_io, mimetype="image/png")        
+            
+            
+        except Exception as e:
+            print("Error: ", e)
+            return jsonify({"error": str(e)}), 500
+        
+@app.route("/health", methods=["GET"])
+def health_check():
+    return jsonify({"status": "mmm", "service": "icon-gen"})       
+        
 if __name__ == "__main__":
     print("Server on 5000")
     app.run(debug=True, host="0.0.0.0", port=5000)
---
+++ config.py
@@ -0,0 +1,5 @@
+BASE_URL="https://itunes.apple.com/search"
+ENTITY = "software"
+MEDIA = "software"
+COUNTRY = "us"
+LIMIT = 1
